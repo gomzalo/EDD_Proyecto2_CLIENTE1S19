1ba7143edd024e836305a2b6c9d3f9d0156d4434
gomzalo <201318652@ingenieria.usac.edu.gt>
2019-04-22 03:56:27 -0600

    Pagina usuarios (completa)

--- dist/js/proyecto2/constantes.js
+++ dist/js/proyecto2/constantes.js
@@ -1,17 +1,14 @@
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::		LOGIN ADMIN		 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       LOGIN ADMIN     ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 const login_admin           =   "http://localhost:8080/proyecto2/api/loginadmin"
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::     USUARIOS    ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       USUARIOS        ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 const new_user              =   "http://localhost:8080/proyecto2/api/newuser";
-const cargar_catedraticoss  =   "http://localhost:8080/proyecto2/api/cargarcatedraticos";
-const get_catedraticos      =   "http://localhost:8080/proyecto2/api/getarbolcatedraticos";
-const edit_catedratico      =   "http://localhost:8080/proyecto2/api/editcatedratico";
-const get_catedraticos64    =   "http://localhost:8080/proyecto2/api/getgraficaarbol64";
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::     RUTAS    ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-const cargar_cursos         =   "http://localhost:8080/proyecto2/api/cargarcursos";
-const get_cursos            =   "http://localhost:8080/proyecto2/api/getlistacursos";
-const edit_curso            =   "http://localhost:8080/proyecto2/api/editcurso";
-const get_cursos64          =   "http://localhost:8080/proyecto2/api/getgraficalista64";
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::     RESERVACIONES    ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-const cargar_salones        =   "http://localhost:8080/proyecto2/api/cargarsalones";
-const get_salones64         =   "http://localhost:8080/proyecto2/api/getgraficamatriz64";
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::     FACURAS    ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+const load_users            =   "http://localhost:8080/proyecto2/api/loadusers";
+const edit_user             =   "http://localhost:8080/proyecto2/api/edituser";
+const delete_user           =   "http://localhost:8080/proyecto2/api/deleteuser";
+const get_users             =   "http://localhost:8080/proyecto2/api/getusers";
+const get_users64           =   "http://localhost:8080/proyecto2/api/getusers64";
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       RUTAS           ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       RESERVACIONES   ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       FACTURAS        ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
