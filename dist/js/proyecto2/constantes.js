88cdc1a2044e3790aeea8a167eaa2e8951969d19
gomzalo <201318652@ingenieria.usac.edu.gt>
2019-04-28 12:11:57 -0600

    Paginas actualizadas, iconos, carga, graficas.

--- dist/js/proyecto2/constantes.js
+++ dist/js/proyecto2/constantes.js
@@ -1,6 +1,7 @@
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       LOGIN ADMIN     ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 const login_admin           =   "http://localhost:8080/proyecto2/api/loginadmin"
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       USUARIOS        ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+const login_user            =   "http://localhost:8080/proyecto2/api/loginuser"
 const new_user              =   "http://localhost:8080/proyecto2/api/newuser";
 const load_users            =   "http://localhost:8080/proyecto2/api/loadusers";
 const edit_user             =   "http://localhost:8080/proyecto2/api/edituser";
@@ -8,7 +9,31 @@ const delete_user           =   "http://localhost:8080/proyecto2/api/deleteuser"
 const get_users             =   "http://localhost:8080/proyecto2/api/getusers";
 const get_users64           =   "http://localhost:8080/proyecto2/api/getusers64";
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       RUTAS           ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       RESERVACIONES   ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
+const new_destination       =   "http://localhost:8080/proyecto2/api/newdestination";
+const load_destinations     =   "http://localhost:8080/proyecto2/api/loaddestinations";
+const new_route             =   "http://localhost:8080/proyecto2/api/newroute";
+const load_routes           =   "http://localhost:8080/proyecto2/api/loadroutes";
+const edit_route            =   "http://localhost:8080/proyecto2/api/editroute";
+const delete_route          =   "http://localhost:8080/proyecto2/api/deleteroute"; // Este si
+const remove_route          =   "http://localhost:8080/proyecto2/api/removeroute"; // Este no :v
+const get_routes            =   "http://localhost:8080/proyecto2/api/getroutes";
+const get_routes64          =   "http://localhost:8080/proyecto2/api/getroutes64";
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       FACTURAS        ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+const new_bill              =   "http://localhost:8080/proyecto2/api/newbill";
+const load_bills            =   "http://localhost:8080/proyecto2/api/loadbills";
+const edit_bill             =   "http://localhost:8080/proyecto2/api/editbill";
+const get_bills             =   "http://localhost:8080/proyecto2/api/getbills";
+const get_bills64           =   "http://localhost:8080/proyecto2/api/getbills64";
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       RESERVACIONES   ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+const new_reservation       =   "http://localhost:8080/proyecto2/api/newreservation";
+const load_reservations     =   "http://localhost:8080/proyecto2/api/loadreservations";
+const get_reservations      =   "http://localhost:8080/proyecto2/api/getreservations";
+const get_reservations64    =   "http://localhost:8080/proyecto2/api/getreservations64";
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       VALIDACIONES    ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+const new_validation        =   "http://localhost:8080/proyecto2/api/newvalidation";
+const load_validations      =   "http://localhost:8080/proyecto2/api/loadvalidations";
+const delete_validation     =   "http://localhost:8080/proyecto2/api/deletevalidation";
+const get_validations       =   "http://localhost:8080/proyecto2/api/getvalidations";
+const get_validations64     =   "http://localhost:8080/proyecto2/api/getvalidations64";
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       GRAFICA COMPLETA    ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+const get_completa64        =   "http://localhost:8080/proyecto2/api/getcompleta64";
